33ebedc1c4c27c3c8eb58d00fb72ef583e56f4ce
Zeph180 <zephrichards1@gmail.com>
2023-01-31 18:15:17 +0300

    Task2

---
+++ reverseString.js
@@ -0,0 +1,7 @@
+function reverseString(str) {
+  return(str.split("").reverse().join(""))
+}
+
+reverseString("hello")
+
+module.exports = reverseString;
--- stringLength.test.js
+++ stringLength.test.js
@@ -1,4 +1,5 @@
 const stringLength = require('./stringLength');
+const reverseString = require('./reverseString');
 
 it('returns a number count', () => {
   expect(stringLength("123456")).toBe(6)
@@ -6,5 +7,9 @@ it('returns a number count', () => {
 
 it('checks if argument is less than 0 or greater than or equal 10', () => {
   expect(() => stringLength("")).toThrow(Error)
+});
+
+it('checks if argument is reversed', () => {
+  expect(reverseString("Hello")).toBe("olleH")
 });
 
